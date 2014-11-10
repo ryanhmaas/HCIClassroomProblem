3e4849bb1eb9b3b89ac32b0bcbd8c080d69d08d3
Ryan Maas <ryan.h.maas@gmail.com>
2014-11-09 19:08:19 -0500

    Fix authentication

--- classroomProblem/app/models/user.rb
+++ classroomProblem/app/models/user.rb
@@ -10,14 +10,13 @@ class User < ActiveRecord::Base
 
 
 
-  def authenticate_password(email, password)
+  def self.authenticate(email, password)
     user = find_by_email(email)
     if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
       user
     else
       nil
     end
-
   end
 
 
